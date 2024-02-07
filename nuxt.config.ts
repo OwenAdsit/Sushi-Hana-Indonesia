b12eb36072801294b14181a2659e9b0b413a80c1
OwenAdsit <owenYangadsit@protonmail.com>
2024-02-07 21:09:14 +0800

    Installing i18n

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  modules: [
+    '@nuxtjs/i18n',
+  ],
   css: ['~/assets/css/tailwind.css'], 
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {}
     }
+  },
+  i18n: {
+    locales: ['id-ID'],
+    defaultLocale: 'id-ID',
+    vueI18n: './i18n.config.js'
   }
 })
